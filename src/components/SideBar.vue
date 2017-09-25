d35e498c5420afc1711c5f699fb077c71ab4f67b
LDQ-first <178848890@qq.com>
2017-09-25 18:56:07 +0800

    'optimize'

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,8 +1,12 @@
 <template>
     <div class="sidebar">
         <div class="avatarWrapper">
-            <img class="border" src="../assets/img/Mask.png" alt="">
-            <div  class="avatar" alt=""></div>
+            <img class="border" src="../assets/img/Mask.png">
+            <div class="avatar" alt="刘德铨" title="刘德铨"></div>
+        </div>
+        <div class="name-job">
+            <h1 class="name">刘德铨</h1>
+            <h2 class="job">前端工程师</h2>
         </div>
     </div>
 </template>
@@ -62,6 +66,22 @@ export default {
                 left: 0px;
                 top: 0px;
                 z-index: 10;
+            }
+        }
+        .name-job {
+            width: 100%;
+            padding: 1em 0;
+            background: rgba(0, 0, 0, 0.3);
+            .name {
+                color: #EEE;
+                font-size: 20px;
+            }
+            .job {
+                font-size: 18px;
+                color: #ED5527;
+                text-shadow: 0 1px 0 #ccc,
+                             0 6px 1px rgba(0,0,0,0.1),
+                             0 0 5px rgba(0,0,0,0.1);
             }
         }
 
